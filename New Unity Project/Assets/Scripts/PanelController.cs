24daa188573a27bbedaca419f58e0641ad73a4c8
sumogri <kazusi@hotmail.co.jp>
2017-11-20 00:14:55 +0900

    [add] UIを仮置

---
+++ New Unity Project/Assets/Scripts/PanelController.cs
@@ -0,0 +1,40 @@
+﻿using System.Linq;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+/// <summary>
+/// 素材パネルのコントローラー
+/// </summary>
+public class PanelController : MonoBehaviour
+{
+    private Vector3 mouseDiff;
+
+    // Use this for initialization
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void OnDown()
+    {
+        mouseDiff = transform.position - Input.mousePosition;
+        transform.SetAsLastSibling();
+    }
+
+    public void OnDrag()
+    {
+        transform.position = Input.mousePosition + mouseDiff;
+        
+    }
+
+    public void OnDrop()
+    {
+    }
+}
